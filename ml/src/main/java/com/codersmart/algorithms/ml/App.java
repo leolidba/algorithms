d61a5ed266865f0ce6f429ea9af966a7f702c4a8
leoli2 <leoli2@ebay.com>
2018-01-26 18:58:50 -0800

    Start Project

---
+++ ml/src/main/java/com/codersmart/algorithms/ml/App.java
@@ -0,0 +1,13 @@
+package com.codersmart.algorithms.ml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
